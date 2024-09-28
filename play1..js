7050f09f645d2b1b6ff9cdf5505e7bbc8cb8c574
MONKEYbutt193 <hanna@lau.fm>
2024-09-28 11:57:04 +1000

    new repository

---
+++ play1..js
@@ -0,0 +1,23 @@
+// meteor catching game
+
+
+function setup() {
+    createCanvas(400, 400);
+    meteorX = 200
+    meteorY = 0
+    
+    
+   }
+   function draw() {
+    background(0, 0, 0);
+    noStroke();
+    // Draw the meteor
+    fill(0, 254, 202);
+    ellipse(200, 0, 20, 20);
+    200, 0 + 1;
+   }
+   
+   
+   // ellipse(mouseX, mouseY,20,20);
+   // let meteorDiameter = 50; 
+   
